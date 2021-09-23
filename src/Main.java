5436d8d7f1f4ed3d3be0bbf24d8f711a186edeb9
pinkyMan79 <terenindanila5@gmail.com>
2021-09-23 14:43:15 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        
+        System.out.println("Hello, GIT");
 
     }
 
